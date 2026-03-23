b26c18c9bd442d68b834ad589f054548c5817c7f
ronanmatheus <71600984+ronanmatheus@users.noreply.github.com>
2026-03-23 10:16:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log("Servidor rodando na porta " + PORT)
 });
